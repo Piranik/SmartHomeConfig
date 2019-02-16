ec048940dedf0a2ffb51cccf82ec832edf8688c2
Thomas Baxter <thomas@thomasbaxter.info>
2019-02-16 17:07:13 +1100

    Setup Dev Environment for HA

--- docker/dockerupdate.sh
+++ docker/dockerupdate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/tbsmarthome/smarthome
-docker-compose down $1 $2 $3
-docker-compose pull $1 $2 $3
-docker-compose up -d $1 $2 $3
+docker-compose down $1
+docker-compose pull $1
+docker-compose up -d $1
---
+++ hass_dev/README.md
@@ -0,0 +1,1 @@
+TBSmartHome - Home Assistant Configuration
